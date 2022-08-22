2dcd57750090cff15cfe1d084845635227642e19
Mohamed Baaziz <mhabaaziz@gmail.com>
2022-08-22 16:49:03 +0200

    :sparkles: search club by id title and tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { Search } from "@mui/icons-material";
 import sports, { Sport } from "./_sports";
 import { grey } from "@mui/material/colors";
 
+const SEARCH_DELAY_MS = 500;
+
 const Container = styled(Box)<BoxProps>(() => ({
   width: "100%",
   height: "100%",
@@ -35,16 +37,57 @@ const SearchContainer = styled(Box)<BoxProps>(() => ({
   gridArea: "search",
 }));
 
+const filterSport: (search: string) => (sport: Sport) => boolean =
+  (search: string) => (sport: Sport) => {
+    return (
+      search.length === 0 ||
+      sport.id.includes(search) ||
+      sport.title.includes(search) ||
+      sport.tags.some((tag) => tag.includes(search))
+    );
+  };
+
 const Home: NextPage = () => {
   const [selectedSport, setSelectedSport] = React.useState<Sport | undefined>(
     undefined
   );
+  const [search, setSearch] = React.useState<string>("");
+  const [searchInputValue, setSearchInputValue] = React.useState<string>("");
+  const [searchTimeout, setSearchTimeout] = React.useState<
+    NodeJS.Timeout | undefined
+  >(undefined);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setSearchInputValue(value);
+    if (searchTimeout) {
+      clearTimeout(searchTimeout);
+    }
+    const timeout = setTimeout(() => {
+      setSearch(value);
+    }, SEARCH_DELAY_MS);
+    setSearchTimeout(timeout);
+  };
+
+  const filteredSports = React.useMemo(
+    () => sports.filter(filterSport(search)),
+    [search]
+  );
+
+  console.log(filteredSports);
 
   return (
     <Container>
       <SearchContainer>
         <Search sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" variant="standard" fullWidth />
+        <TextField
+          id="input-with-sx"
+          variant="standard"
+          fullWidth
+          value={searchInputValue}
+          onChange={handleSearchChange}
+        />
       </SearchContainer>
       <Box
         sx={{
@@ -56,7 +99,7 @@ const Home: NextPage = () => {
           gridGap: "10px 10px",
         }}
       >
-        {sports.map((sport) => (
+        {filteredSports.map((sport) => (
           <Button
             key={sport.id}
             onClick={() => {
